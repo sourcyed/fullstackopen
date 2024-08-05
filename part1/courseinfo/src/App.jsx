1896975afa132e3368860fd9934e8d7dca079053
sourcyed <31018609+sourcyed@users.noreply.github.com>
2024-08-05 14:49:46 +0300

    Finished part1 ex1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -19,21 +19,24 @@ const Content = (props) => {
   )
 }
 
-const Total = (props) => (
-  <p>Number of exercises {props.total}</p>
-)
+const Total = (props) => {
+  const total = props.parts.map(part => part.exercises).reduce((acc, x) => acc+x, 0)
+  return <p>Number of exercises {total}</p>
+}
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {name: 'Fundementals of React', exercises: 10}
-  const part2 = {name: 'Using props to pass date', exercises: 7}
-  const part3 = {name: 'State of a component', exercises: 14}
+  const parts = [
+    {name: 'Fundementals of React', exercises: 10},
+    {name: 'Using props to pass date', exercises: 7},
+    {name: 'State of a component', exercises: 14}
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={[part1, part2, part3]}/>
-      <Total total={part1.exercises + part2.exercises + part3.exercises}/>
+      <Content parts={parts}/>
+      <Total parts={parts}/>
     </div>
   )
 }
