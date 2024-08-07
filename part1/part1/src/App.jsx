4490071812d2824fe6e3aecb553c64fd6776148c
sourcyed <31018609+sourcyed@users.noreply.github.com>
2024-08-07 16:11:00 +0300

    Finished part1.d

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -15,32 +15,54 @@ const Display = ({counter}) => <div>{counter}</div>
 
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
+const History = ({allClicks}) => {
+  if (allClicks.length === 0) {
+    return (
+      <div>the app is used by pressing the buttons</div>
+    )
+  }
+  return (
+    <div>button press history: {allClicks.join(' ')}</div>
+  )
+}
+
 const App = () => {
 
-  const [counter, setCounter] = useState(0)
-  console.log('rendering with counter value', counter)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
-  const increaseByOne = () => {
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right) 
   }
 
-  const decreaseByOne = () => { 
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  }
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'));
+    const updatedRight = right + 1;
+    setRight(updatedRight);
+    setTotal(left + updatedRight);
+  };
 
-  const setToZero = () => {
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
+  const handleResetClick = () => {
+    setAll([])
+    setLeft(0)
+    setRight(0)
+    setTotal(0)
   }
 
   return (
     <>
-      <Display counter={counter}/>
-      <Button onClick={increaseByOne} text='+'/>
-      <Button onClick={setToZero} text='0'/>
-      <Button onClick={decreaseByOne} text='-'/> 
+      {left}
+      <Button onClick={handleLeftClick} text='left'/>
+      <Button onClick={handleResetClick} text='reset'/>
+      <Button onClick={handleRightClick} text='right'/>
+      {right}
+      <History allClicks={allClicks}/>
+      <p>total {total}</p>
     </>
   )
 }
