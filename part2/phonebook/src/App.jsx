34379380227a06bf3702e42a47dbf719e61af4bc
sourcyed <31018609+sourcyed@users.noreply.github.com>
2024-08-11 00:30:15 +0300

    Finished ex 2.16

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,18 +2,25 @@ import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import Notification from './components/Notification'
 import personService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([])
-
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterName, setFilter] = useState('')
+  const [notification, setNotification] = useState(null)
 
   useEffect(() => {
     personService.getAll().then(p => setPersons(p))
   }, [])
+
+  const popNotification = (message, type) => {
+    const n = { message, type }
+    setNotification(n)
+    setTimeout(() => setNotification(null) , 5000)
+  }
 
   const addPerson = event => {
     event.preventDefault()
@@ -29,10 +36,15 @@ const App = () => {
     if (duplicate) {
       newPerson.id = duplicate.id
       window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)
-        personService.update(newPerson).then(nP => setPersons(persons.map(p => p.id !== nP.id ? p : nP)))
+        personService.update(newPerson)
+          .then(nP => setPersons(persons.map(p => p.id !== nP.id ? p : nP)))
+          .catch(() => popNotification(`Information of ${newName} has already been removed from server`, 'error'))
     }
-    else
-      personService.create(newPerson).then(p => setPersons(persons.concat(p)))
+    else {
+      personService.create(newPerson)
+        .then(p => setPersons(persons.concat(p)))
+        .then(() => popNotification('Added ' + newName, 'confirmation'))
+    }
 
     setNewName('')
     setNewNumber('')
@@ -42,13 +54,16 @@ const App = () => {
 
   const delHandler = pToDel => () => {
     if (window.confirm(`Delete ${pToDel.name} ?`))
-      personService.del(pToDel.id).then(setPersons(persons.filter(p => p.id !== pToDel.id)))
+      personService.del(pToDel.id)
+        .then(setPersons(persons.filter(p => p.id !== pToDel.id)))
+        .catch(() => popNotification(`${pToDel.name} is already deleted`, 'error'))
   }
 
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification notification={notification}/>
       <Filter filterName={filterName} onChange={handleInput(setFilter)}/>
 
       <h3>add a new</h3>
