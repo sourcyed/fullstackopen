bb82ff169dd75bad7c63d6b33dc9a499531abdcd
sourcyed <31018609+sourcyed@users.noreply.github.com>
2024-10-29 12:40:56 +0200

    Separate blog model to its own module

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -3,15 +3,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require('./models/blog')
 
 const mongoUrl = config.MONGODB_URI || 'mongodb://localhost/bloglist'
 mongoose.connect(mongoUrl)
