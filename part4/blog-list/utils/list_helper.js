e2054941135bca722c27a5a9d36c7ddd3a1fb893
sourcyed <31018609+sourcyed@users.noreply.github.com>
2024-10-29 21:06:08 +0200

    Add totalLikes function

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, x) => sum + x.likes, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
