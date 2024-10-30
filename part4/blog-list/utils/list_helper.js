583c4e68e11080dd6d5e8fc1eb8eb126b62e9192
sourcyed <31018609+sourcyed@users.noreply.github.com>
2024-10-30 14:07:41 +0200

    Added favoriteBlog function

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -6,7 +6,12 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, x) => sum + x.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((max, x) => x.likes > max ? x.likes : max, 0)
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
