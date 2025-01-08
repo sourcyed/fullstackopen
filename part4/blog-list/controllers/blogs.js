4a7e5c405f4c85a2d3e9bd446d685e842d3a346d
sourcyed <31018609+sourcyed@users.noreply.github.com>
2025-01-08 18:47:47 +0200

    Complete ex4.19

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
@@ -7,19 +8,32 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
-  console.log(request.body)
   if (!request.body)
     return response.status(400).end()
   if (!request.body.title || !request.body.url)
     return response.status(400).end()
-  if (!request.body.user)
-    request.body.user = await User.findOne({}).schemaLevelProjections(x => x.id)
+
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
+  request.body.user = user
+  console.log(request.body)
 
   const blog = new Blog(request.body)
 
   const result = await blog.save()
-  const user = await User.findById(request.body.user)
   user.blogs = user.blogs.concat(result._id)
   await user.save()
   response.status(201).json(result)
--- part4/blog-list/utils/middleware.js
+++ part4/blog-list/utils/middleware.js
@@ -11,6 +11,8 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).json({ error: 'expected `username` to be unique'})
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({ error: 'token invalid' })
   }
 }
 
