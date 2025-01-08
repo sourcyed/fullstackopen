5d5248d0c529cdb02dfca590e09078c14a9875cd
sourcyed <31018609+sourcyed@users.noreply.github.com>
2025-01-08 18:01:03 +0200

    Complete ex4.19

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 
 const mongoUrl = config.MONGODB_URI || 'mongodb://localhost/bloglist'
@@ -16,6 +17,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
