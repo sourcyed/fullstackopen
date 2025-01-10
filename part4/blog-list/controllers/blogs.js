8170e23779f8aaaf4077b26b52c9b01e20e01ae7
sourcyed <31018609+sourcyed@users.noreply.github.com>
2025-01-10 12:38:43 +0200

    Complete ex4.20

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -8,21 +8,14 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
-  }
-  return null
-}
-
 blogsRouter.post('/', async (request, response) => {
   if (!request.body)
     return response.status(400).end()
   if (!request.body.title || !request.body.url)
     return response.status(400).end()
 
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  // console.log(request.token)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'token invalid' })
   }
--- part4/blog-list/utils/middleware.js
+++ part4/blog-list/utils/middleware.js
@@ -16,4 +16,13 @@ const errorHandler = (error, request, response, next) => {
   }
 }
 
-module.exports = { unknownEndpoint, errorHandler }
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '')
+  }
+
+  next()
+}
+
+module.exports = { unknownEndpoint, errorHandler, tokenExtractor }
