db84a8b8a16d40ca0457256b794d82954f6135fe
sourcyed <31018609+sourcyed@users.noreply.github.com>
2025-06-21 21:55:06 +0300

    add username to logged in screen

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -29,6 +29,7 @@ const App = () => {
     user ?
     <div>
       <h2>blogs</h2>
+      <p>{user.username} logged in</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
