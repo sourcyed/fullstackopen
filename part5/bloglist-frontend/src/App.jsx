e165312af0de4c85104b7a8e8cfc8ecba1e8a7de
sourcyed <31018609+sourcyed@users.noreply.github.com>
2025-06-21 22:28:41 +0300

    add info box to part5 exercise bloglist frontend

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import { useState, useEffect, useRef } from 'react'
+import './App.css'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import axios from 'axios'
+import InfoBox from './components/InfoBox'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
+
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedUser')
@@ -36,32 +40,39 @@ const App = () => {
   }
 
   return (
-    user ?
     <div>
-      <h2>blogs</h2>
-      <p>{user.username} logged in</p>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-      <p>
-        <button onClick={() => { window.localStorage.removeItem('loggedUser'); setUser(null); }}>logout</button>
-      </p>
-    </div> :
-    <div>
-      <form onSubmit={handleLogin}>
-        <h2>log in to application</h2>
-        <p>
-        username
-        <input type="text" value={username} onChange={e => setUsername(e.currentTarget.value)} />
-        </p>
-        <p>
-        password
-        <input type="password" value={password} onChange={e => setPassword(e.currentTarget.value)} />
-        </p>
-        <p>
-        <input type="submit" value="login" />
-        </p>
-      </form>
+      <InfoBox message={errorMessage} />
+
+      {user ?
+        <div>
+          <h2>blogs</h2>
+          <p>{user.username} logged in</p>
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+          )}
+          <p>
+            <button onClick={() => { window.localStorage.removeItem('loggedUser'); setUser(null); }}>logout</button>
+          </p>
+        </div>
+        :
+        <div>
+          <form onSubmit={handleLogin}>
+            <h2>log in to application</h2>
+            <p>
+              username
+              <input type="text" value={username} onChange={e => setUsername(e.currentTarget.value)} />
+            </p>
+            <p>
+              password
+              <input type="password" value={password} onChange={e => setPassword(e.currentTarget.value)} />
+            </p>
+            <p>
+              <input type="submit" value="login" />
+            </p>
+          </form>
+        </div>
+      }
+
     </div>
   )
 }
---
+++ part5/bloglist-frontend/src/components/InfoBox.jsx
@@ -0,0 +1,13 @@
+const InfoBox = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className='error'>
+      {message}
+    </div>
+  )
+}
+
+export default InfoBox
