9a575ecbbe3fa8cc51205cf5da54388fc5458ded
sourcyed <31018609+sourcyed@users.noreply.github.com>
2025-06-25 19:02:35 +0300

    complete ex5.7

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -83,11 +83,11 @@ const App = () => {
             <BlogForm handleCreate={handleCreate} />
           </Togglable>
 
-          <ul>
+          <div>
             {blogs.map(blog =>
               <Blog key={blog.id} blog={blog} />
             )}
-          </ul>
+          </div>
         </div>
         :
         <div>
--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,21 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import Togglable from './Togglable'
+
+const Blog = ({ blog }) => {
+  return (
+    <div style={{ border: '1px solid black', margin: '10px 10px 10px 10px', padding: '10px 10px 10px 10px' }}>
+      <p>
+        {blog.title}
+      </p>
+      <Togglable buttonLabel='view'>
+        <p>
+          {blog.url} <button>like</button>
+        </p>
+        <p>
+          {blog.author}
+        </p>
+      </Togglable>
+    </div>
+  )
+}
 
 export default Blog
