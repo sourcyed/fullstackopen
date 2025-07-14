a6a2b099b3abe8aafb709d7b97c2a2a37b095f57
sourcyed <31018609+sourcyed@users.noreply.github.com>
2025-07-14 20:41:44 +0300

    remover redundant code

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,15 +1,7 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { voteAnecdote } from './reducers/anecdoteReducer'
-
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-  const anecdotes = useSelector(state => state.sort((a,b) => b.votes - a.votes))
-  const dispatch = useDispatch()
-
-
-
   return (
     <div>
       <h2>Anecdotes</h2>
